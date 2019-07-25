19438e8386765c46140f6e80cd7ca938adf8d095
Russell Joyce <russell.joyce@york.ac.uk>
2019-07-25 16:51:14 +0100

    Moved UART2 code to its own .c/.h files

--- pi-puck-e-puck1.X/main.c
+++ pi-puck-e-puck1.X/main.c
@@ -4,19 +4,14 @@
 #include "system.h"
 #include <libpic30.h>
 #include "leds.h"
-#include <uart.h>
+#include "uart2.h"
 #include <stdio.h>
 
-// Use UART2 for stdio
-int __C30_UART = 2;
 
 int16_t main(void)
 {
-    // Set up UART2
-    U2BRG = 7; // 115200 bps
-    U2MODE = UART_EN & UART_IDLE_STOP & UART_DIS_WAKE & UART_DIS_LOOPBACK & UART_DIS_ABAUD & UART_NO_PAR_8BIT & UART_1STOPBIT;
-    U2STA = UART_INT_TX_BUF_EMPTY & UART_TX_PIN_NORMAL & UART_TX_ENABLE & UART_INT_RX_BUF_FUL & UART_ADR_DETECT_DIS & UART_RX_OVERRUN_CLEAR;
-
+    uart2_init();
+    
     printf("I am an e-puck.\n");
     
     leds_init();
