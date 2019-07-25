4d44b40950a579ed824a8f0e60d22b3727a8218a
Russell Joyce <russell.joyce@york.ac.uk>
2019-07-25 18:41:34 +0100

    Added setting of LEDs from I2C registers 0 and 1
    Register 0 sets all 8 outer LEDs (one per bit).
    Register 1 sets front and body LEDs (with bits 0 and 1 respectively).

--- pi-puck-e-puck1.X/main.c
+++ pi-puck-e-puck1.X/main.c
@@ -10,45 +10,31 @@
 
 int16_t main(void)
 {
+    int i;
+    for (i = 0; i < 256; i++) {
+        i2c_data[i] = 0;
+    }
+    
     uart2_init();
+    i2c_slave_init();
+    leds_init();
     
     printf("I am an e-puck.\n");
     
-    unsigned int i;
-    for (i = 0; i < 256; i++) {
-        i2c_data[i] = i;
-    }
-    
-    i2c_slave_init();
-    
-    leds_init();
-    unsigned long delay = 1000/8;
-    while (1)
-    {
-        // Loop through LEDs
-        LED0 = 1;
-        __delay_ms(delay);
-        LED0 = 0;
-        LED1 = 1;
-        __delay_ms(delay);
-        LED1 = 0;
-        LED2 = 1;
-        __delay_ms(delay);
-        LED2 = 0;
-        LED3 = 1;
-        __delay_ms(delay);
-        LED3 = 0;
-        LED4 = 1;
-        __delay_ms(delay);
-        LED4 = 0;
-        LED5 = 1;
-        __delay_ms(delay);
-        LED5 = 0;
-        LED6 = 1;
-        __delay_ms(delay);
-        LED6 = 0;
-        LED7 = 1;
-        __delay_ms(delay)
-        LED7 = 0;
+    uint8_t leds_outer = 0;
+    uint8_t leds_front_body = 0;
+    leds_set_outer(leds_outer);
+    leds_set_front_body(leds_front_body);
+
+    while (1) {
+        if (leds_outer != i2c_data[0]) {
+            leds_outer = i2c_data[0];
+            leds_set_outer(leds_outer);
+        }
+        if (leds_front_body != i2c_data[1]) {
+            leds_front_body = i2c_data[1];
+            leds_set_front_body(leds_front_body);
+        }
+        __delay_ms(1);
     }
 }
