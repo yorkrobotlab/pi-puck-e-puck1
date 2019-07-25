3f6edc01ace1fdc2e9d5c5ba5e9556fa795e2e93
Russell Joyce <russell.joyce@york.ac.uk>
2019-07-25 18:52:01 +0100

    Improved formatting of UART2 register set-up

--- pi-puck-e-puck1.X/uart2.c
+++ pi-puck-e-puck1.X/uart2.c
@@ -5,9 +5,23 @@
 // Use UART2 for stdio
 int __C30_UART = 2;
 
-void uart2_init() {
+void uart2_init()
+{
     // Set up UART2
     U2BRG = 7; // 115200 bps
-    U2MODE = UART_EN & UART_IDLE_STOP & UART_DIS_WAKE & UART_DIS_LOOPBACK & UART_DIS_ABAUD & UART_NO_PAR_8BIT & UART_1STOPBIT;
-    U2STA = UART_INT_TX_BUF_EMPTY & UART_TX_PIN_NORMAL & UART_TX_ENABLE & UART_INT_RX_BUF_FUL & UART_ADR_DETECT_DIS & UART_RX_OVERRUN_CLEAR;
+    
+    U2MODE = UART_EN &
+             UART_IDLE_STOP &
+             UART_DIS_WAKE &
+             UART_DIS_LOOPBACK &
+             UART_DIS_ABAUD &
+             UART_NO_PAR_8BIT &
+             UART_1STOPBIT;
+    
+    U2STA = UART_INT_TX_BUF_EMPTY &
+            UART_TX_PIN_NORMAL &
+            UART_TX_ENABLE &
+            UART_INT_RX_BUF_FUL &
+            UART_ADR_DETECT_DIS &
+            UART_RX_OVERRUN_CLEAR;
 }
